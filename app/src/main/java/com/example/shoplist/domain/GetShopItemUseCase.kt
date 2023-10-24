a7827fb425d8280b99a2efad3672bd681513aae6
Popov Nicolai <110418111+psyrut1983@users.noreply.github.com>
2023-10-24 15:52:00 +0300

    domain part 2

--- app/src/main/java/com/example/shoplist/domain/GetShopItemUseCase.kt
+++ app/src/main/java/com/example/shoplist/domain/GetShopItemUseCase.kt
@@ -1,7 +1,7 @@
 package com.example.shoplist.domain
 
-class GetShopItemUseCase {
+class GetShopItemUseCase (private val shopListRepository: ShopListRepository){
     fun getShopItem (shopItemId: Int): ShopItem{
-TODO()
+    return shopListRepository.getShopItem(shopItemId)
     }
 }
