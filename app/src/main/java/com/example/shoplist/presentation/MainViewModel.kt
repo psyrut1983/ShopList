56f5b8727e93083911abc2240fe43d2b2cf4f42e
Popov Nicolai <110418111+psyrut1983@users.noreply.github.com>
2023-10-27 11:33:14 +0300

    config app now is running

---
+++ app/src/main/java/com/example/shoplist/presentation/MainViewModel.kt
@@ -0,0 +1,28 @@
+package com.example.shoplist.presentation
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.shoplist.data.ShopListRepositoryImpl
+import com.example.shoplist.domain.DeleteShopItemUseCase
+import com.example.shoplist.domain.EditShopItemUseCase
+import com.example.shoplist.domain.GetShopListUseCase
+import com.example.shoplist.domain.ShopItem
+
+
+class MainViewModel: ViewModel()  {
+    private val repository = ShopListRepositoryImpl
+
+    private val getShopListUseCase = GetShopListUseCase(repository)
+    private val deleteShopItemUseCase = DeleteShopItemUseCase(repository)
+    private val editShopItemUseCase = EditShopItemUseCase(repository)
+
+    val shopList = MutableLiveData<List<ShopItem>>()
+
+    fun getShopList(){
+        val list = getShopListUseCase.getShopList()
+        shopList.value = list
+
+    }
+
+}
