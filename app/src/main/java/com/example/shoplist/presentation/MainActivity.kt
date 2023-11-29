754f62a0610360bf651f69f9aa92d6c651412cb7
Popov Nicolai <110418111+psyrut1983@users.noreply.github.com>
2023-11-29 17:37:15 +0300

    presentation maket

--- app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
+++ app/src/main/java/com/example/shoplist/presentation/MainActivity.kt
@@ -9,6 +9,8 @@ import com.example.shoplist.R
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
 
+    private var count = 0
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,7 +18,12 @@ class MainActivity : AppCompatActivity() {
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         viewModel.shopList.observe(this) {
             Log.d("Test", it.toString())
+            if (count == 0 ) {
+                count++
+                val item = it[0]
+                viewModel.changeEnableState(item)
+            }
         }
-        viewModel.getShopList()
+
     }
 }
